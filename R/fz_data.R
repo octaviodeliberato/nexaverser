1f4a485b3fc40bc57b0acd642520cd8b04e188d7
octaviodeliberato <octavio.deliberato@gmail.com>
2022-10-26 12:14:27 -0300

    Update fz_data.rda

--- R/fz_data.R
+++ R/fz_data.R
@@ -1,8 +1,8 @@
-#' Data from 14 tags.
+#' Data from 18 tags.
 #'
-#' A dataset containing PIMS data (daily averages) from 14 tags.
+#' A dataset containing PIMS data (daily averages) from 18 tags.
 #'
-#' @format A data frame with 1740 observations and 15 variables:
+#' @format A data frame with 1738 observations and 19 variables:
 #' \describe{
 #'   \item{date}{Measurement date, yyy-mm-dd format}
 #'   \item{fz_s_0033_zn2}{Conc Zn Filtrado Secundario - S033}
@@ -10,8 +10,11 @@
 #'   \item{fz_fit_023}{Vazao Filtro S034}
 #'   \item{fz_cons_floc_flonex}{Consumo Diario de Floculante Flonex 9026 SNF}
 #'   \item{fz_pit_012_h}{PRESSAO DE VACUO DA BOMBA K001}
+#'   \item{fz_tit_413}{Temperatura do Tanque 413}
 #'   \item{fz_ait_002_h}{Media hora PH TANQ B469}
+#'   \item{fz_fit_030}{Adicao leite de cal no tanque 469}
 #'   \item{fz_filtrabilidade_precip}{Filtrabilidade do ppFe}
+#'   \item{lx_fit_085}{Transmissor de Vazao Bomba P085}
 #'   \item{lx_b043_fe_2}{Concentracao de ferroso no ultimo tanque (B-43) Fe+2 B043}
 #'   \item{lx_b_35_fet}{Ferro Total no tanque 35}
 #'   \item{lx_li_claro_s026a}{Nivel de claro espessador S026A}
@@ -19,7 +22,7 @@
 #'   \item{lx_b022_h}{acidez TQ 21}
 #'   \item{lx_b021_fe_t}{Ferro Total B021}
 #'   \item{lx_b043_fet}{Ferro Total B043}
-#'   ...
+#'   \item{lb_fz_filtros033silw_zn}{Teor de Zinco na Torta do Filtro S033}
 #' }
-#' @source \url{www.linkedin.com/in/odneto}
+#' @source \url{www.aggxtream.com}
 "fz_data"
--- data-raw/fz_data.R
+++ data-raw/fz_data.R
@@ -1,14 +1,60 @@
 ## code to prepare `fz_data` dataset goes here
-fz_data <- rio::import("data-raw/Dados-FZ-5-anos.xlsx")
+fz_data_1 <- readxl::read_xlsx("data-raw/TAGS FZ.xlsx", sheet = 1, skip = 2)
 
-fz_data <- fz_data |>
+names(fz_data_1) |>
+  grep(pattern = "...", fixed = TRUE) -> .
+
+value_col_names <- setdiff(names(fz_data_1), names(fz_data_1)[.])
+
+value_col_names |>
+  grep(pattern = "^[0-9]", invert = TRUE) -> .
+
+value_col_names <- value_col_names[.] |>
+  janitor::make_clean_names()
+
+cols_to_keep <- colSums(is.na(fz_data_1)) < nrow(fz_data_1)
+
+rows_to_keep <- rowSums(is.na(fz_data_1)) < ncol(fz_data_1)
+
+dates <- fz_data_1[rows_to_keep, 2] |> dplyr::pull(1)
+
+fz_data_1 <- fz_data_1[rows_to_keep, cols_to_keep] |>
+  dplyr::select(tidyselect::where(is.character)) |>
+  dplyr::mutate(date = as.Date(dates)) |>
   janitor::clean_names() |>
-  dplyr::rename(date = data)
+  dplyr::relocate(date) |>
+  purrr::set_names(c("date", value_col_names)) |>
+  dplyr::mutate(dplyr::across(tidyselect::where(is.character), as.numeric)) |>
+  timetk::summarise_by_time(
+    .date_var = date,
+    .by = "day",
+    dplyr::across(tidyselect::where(is.numeric), ~mean(.x, na.rm = TRUE))
+  )
 
-perc_na <- round(colSums(is.na(fz_data)) / nrow(fz_data) * 100, 0)
+fz_data_2 <- readxl::read_excel("data-raw/Dados-FZ-5-anos.xlsx")
 
-keep <- perc_na < 30
+fz_data_2 <- fz_data_2 |>
+  dplyr::mutate(Data = as.Date(Data)) |>
+  dplyr::rename(date = Data) |>
+  timetk::filter_by_time(.date_var = date, .start_date = fz_data_1$date[1], .end_date = fz_data_1$date |> utils::tail(1)) |>
+  janitor::clean_names()
 
-fz_data <- fz_data[keep]
+fz_data_3 <- purrr::map2_df(
+  .x = fz_data_1 |> dplyr::select(-date),
+  .y = fz_data_2 |> dplyr::select(-date),
+  .f = \(.x, .y) {
+    dt <- tibble::tibble(x = .x, y = .y)
+    dm <- apply(X = dt, MARGIN = 1, FUN = mean, na.rm = TRUE)
+    return(dm)
+  }
+)
+
+fz_data <- dplyr::bind_cols(date = fz_data_1$date, fz_data_3)
+
+# cols_to_keep <- (colSums(is.na(fz_data)) / nrow(fz_data)) < 1/3
+#
+# table(cols_to_keep)
+#
+# fz_data <- fz_data[, cols_to_keep]
 
 usethis::use_data(fz_data, overwrite = TRUE)
